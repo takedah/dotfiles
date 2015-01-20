2f91a30b6c8b2177e08dfc7fccb3cc94221ca3f4
takedah <takedahiroki@gmail.com>
2015-01-21 01:10:40 +0900

    fix

--- symbolic-link.sh
+++ symbolic-link.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+mkdir -p ~/.vim/bundle
+git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+git clone https://github.com/Shougo/vimproc ~/.vim/bundle/vimproc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
