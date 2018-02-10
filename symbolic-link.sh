27597eb4c8e0dd537f92d0ce884bc53e5a6e4a9f
Hiroki Takeda <takedahiroki@gmail.com>
2018-02-10 13:25:07 +0900

    fixed.

--- symbolic-link.sh
+++ symbolic-link.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
+ln -sf ~/dotfiles/.bash_aliases ~/.bash_aliases
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
