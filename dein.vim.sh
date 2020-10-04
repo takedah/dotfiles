cbdc9ba193e10891e2c97ba57eb4863a55b7ba92
Hiroki Takeda <takedahiroki@gmail.com>
2020-10-04 12:05:57 +0900

    fixed.

--- dein.vim.sh
+++ dein.vim.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
 sh ./installer.sh ~/.cache/dein
+pip install --user jedi
+pip install pynvim
