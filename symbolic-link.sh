d3ff1ddd69b9abadf62bcbf5166af7dd553c1783
Hiroki Takeda <takedahiroki@gmail.com>
2020-10-11 20:15:09 +0900

    fixed.

--- symbolic-link.sh
+++ symbolic-link.sh
@@ -3,4 +3,5 @@ ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.bash_aliases ~/.bash_aliases
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+mkdir -p ~/.config/nvim
 ln -sf ~/dotfiles/.config/nvim ~/.config/nvim
